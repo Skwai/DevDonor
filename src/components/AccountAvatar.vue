0cb07b49186cd06b8f460d6a4fec4d4397201f42
Cameron <cameron@biteable.com>
2017-09-17 20:43:38 +1000

    refactor menu

---
+++ src/components/AccountAvatar.vue
@@ -0,0 +1,20 @@
+<template>
+  <img class="AccountAvatar" :src="picture">
+</template>
+
+<script>
+export default {
+  props: ['picture']
+}
+</script>
+
+<style lang="stylus">
+@require "../styles/config.styl"
+
+.AccountAvatar
+  width: 2.5rem
+  height: 2.5rem
+  border-radius: 50%
+  box-shadow: #fff 0 0 0 1px
+
+</style>
---
+++ src/components/AccountMenu.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="AccountMenu" :class="{ '-show': showAccountMenu }">
+    <button
+      @click.prevent="toggleAccountMenu"
+      class="AccountMenu__Toggle"
+    ><AccountAvatar :picture="getProfile.picture" /></button>
+    <div class="AccountMenu__Options">
+      <a href="#" class="AccountMenu__Option">Your Profile</a>
+      <a href="#" class="AccountMenu__Option">Logout</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import AccountAvatar from '@/components/AccountAvatar'
+
+export default {
+  components: {
+    AccountAvatar
+  },
+
+  computed: {
+    ...mapGetters([
+      'getProfile'
+    ])
+  },
+
+  mounted () {
+    this.onDocumentClick = this.onDocumentClick.bind(this)
+    document.addEventListener('click', this.onDocumentClick)
+  },
+
+  destroyed () {
+    document.removeEventListener('click', this.onDocumentClick)
+  },
+
+  methods: {
+    onDocumentClick (ev) {
+      if (!this.$el.contains(ev.target)) {
+        this.showAccountMenu = false
+      }
+    },
+
+    toggleAccountMenu () {
+      this.showAccountMenu = !this.showAccountMenu
+    }
+  },
+
+  data () {
+    return {
+      showAccountMenu: false
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+@require "../styles/config.styl"
+
+.AccountMenu
+  position: relative
+
+  &__Toggle
+    background: none
+    border: 0
+    padding: 0
+
+  &__Options
+    position: absolute
+    background: colorDarkBlue
+    top: 100%
+    transform: translate(1px, 0) scale(.7, .7)
+    transform-origin: right top
+    right: 0
+    min-width: 10rem
+    box-shadow: rgba(0,0,0,.1) 0 2px 5px
+    transition: transitionBase
+    opacity: 0
+
+    .-show &
+      transform: translate(1px, 0.5rem) scale(1, 1)
+      opacity: 1
+
+    &::after
+      size = 0.5rem
+      content: ""
+      position: absolute
+      top: 0
+      right: calc(1.25rem + 1px)
+      transform: translate(50%, -100%)
+      border-bottom: colorDarkBlue solid size
+      border-left: transparent solid size
+      border-right: transparent solid size
+
+  &__Option
+    display: block
+    color: #fff
+    font-size: 0.875rem
+    white-space: nowrap
+    padding: 1rem spacingBase
+
+    &:hover
+      color: colorPrimaryBlue
+</style>
