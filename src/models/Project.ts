997cddaf3936e0b975267ae1ba12c8e5a35d8683
Cameron <cameron@biteable.com>
2018-02-25 00:52:15 +1100

    added props interface

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,18 +1,34 @@
+export interface IProjectProperties {
+  id?: string
+  title?: string
+  description?: string
+  projectType?: string
+  country?: string
+  state?: string
+  createdAt?: Date
+  organizationName?: string
+  organizationType?: string
+  organizationLogo?: string
+  organizationDescription?: string
+  email?: string
+}
+
 export default class Project {
-  public id?: string | null
+  public id?: string
+  public createdAt?: Date
 
   public title: string = ''
   public description: string = ''
   public projectType: string = ''
   public country: string = ''
-  public createdAt: Date = new Date()
+  public state: string = ''
   public organizationName: string = ''
   public organizationType: string = ''
   public organizationLogo: string = ''
   public organizationDescription: string = ''
   public email: string = ''
 
-  constructor(args = {}) {
-    Object.assign(this, args)
+  constructor(props: IProjectProperties = {}) {
+    Object.assign(this, props)
   }
 }
