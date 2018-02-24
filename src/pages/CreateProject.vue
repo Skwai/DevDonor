05aaafca4d03971516bd8db6d5da4c9385ca7979
Cameron <cameron@biteable.com>
2018-02-25 00:52:27 +1100

    better create project form

--- src/pages/CreateProject.vue
+++ src/pages/CreateProject.vue
@@ -13,16 +13,27 @@
             label="Organization name"
             :required="true"
             :span="2"
-            :valid="validations.step1.organizationName"
+            :valid="validations[STEP_1].organizationName"
             v-model="project.organizationName"
           />
 
+          <AppField
+            type="email"
+            label="Your email address"
+            :span="2"
+            v-model="project.email"
+            :valid="validations[STEP_2].email"
+            :required="true"
+            description="This will be used to send you notifications about your project"
+          />
+
           <AppSelect
-            label="Organization type"
+            label="Charity type"
             :required="true"
             v-model="project.organizationType"
+            :span="2"
             :options="causeOptions"
-            :valid="validations.step1.organizationType"
+            :valid="validations[STEP_1].organizationType"
             description="What goal or cause does your organization focus on?"
           ></AppSelect>
 
@@ -31,7 +42,15 @@
             :required="true"
             v-model="project.country"
             :options="countryOptions"
-            :valid="validations.step1.country"
+            :valid="validations[STEP_1].country"
+          />
+
+          <AppSelect
+            label="State/Province"
+            :required="true"
+            v-model="project.state"
+            :options="countryOptions"
+            :valid="validations[STEP_1].state"
           />
 
           <AppUpload
@@ -42,7 +61,7 @@
             v-model="project.organizationLogo"
             label="Upload your logo"
             description="Upload a picture to use as your logo"
-            :valid="validations.step1.organizationDescription"
+            :valid="validations[STEP_1].organizationDescription"
           />
 
           <AppField
@@ -51,7 +70,9 @@
             :span="2"
             label="Organization description"
             v-model="project.organizationDescription"
-            :valid="validations.step1.organizationDescription"
+            :valid="validations[STEP_1].organizationDescription"
+            :minlength="10"
+            :maxlength="200"
             description="Describe what your organization does, how it helps people, and what its mission is."
           />
         </AppFieldGroup>
@@ -62,7 +83,7 @@
             @click="nextStep"
             color="primary"
             size="large"
-            :disabled="!isValid.step1"
+            :disabled="!isValid[STEP_1]"
           >Next step</AppBtn>
         </div>
       </div>
@@ -73,10 +94,10 @@
         </h2>
         <AppFieldGroup>
           <AppField
-            label="Project title"
+            label="Project name"
             :required="true"
             :span="2"
-            :valid="validations.step2.title"
+            :valid="validations[STEP_2].title"
             v-model="project.title"
             :minlength="10"
             :maxlength="100"
@@ -87,7 +108,7 @@
             v-model="project.projectType"
             :required="true"
             :span="2"
-            :valid="validations.step2.projectType"
+            :valid="validations[STEP_2].projectType"
             :options="projectTypeOptions"
             description="What sort of project are you looking to create?"
           />
@@ -98,22 +119,14 @@
             :span="2"
             label="Project description"
             v-model="project.description"
-            :valid="validations.step2.description"
+            :valid="validations[STEP_2].description"
             :required="true"
             :minlength="50"
-          />
-
-          <AppField
-            type="email"
-            label="Your email address"
-            v-model="project.email"
-            :valid="validations.step2.email"
-            :required="true"
           />
         </AppFieldGroup>
         <div :class="$style.CreateProject__Actions">
           <AppBtn type="button" @click="prevStep">Previous Step</AppBtn>
-          <AppBtn type="submit" color="primary" size="large" :disabled="!isValid.step1 || !isValid.step2">Create My Project</AppBtn>
+          <AppBtn type="submit" color="primary" size="large" :disabled="!isValid[STEP_1] || !isValid[STEP_2]">Create My Project</AppBtn>
         </div>
       </div>
     </form>
@@ -139,8 +152,11 @@ const CAUSE_OPTIONS = [
 ]
 
 const PROJECT_TYPE_OPTIONS = ['Web', 'Mobile', 'Desktop']
+
+// the names of the steps
 const STEP_1 = 'step1'
 const STEP_2 = 'step2'
+const STEPS = [STEP_1, STEP_2]
 
 @Component
 export default class CreateProjectPage extends Vue {
@@ -150,6 +166,8 @@ export default class CreateProjectPage extends Vue {
   private causeOptions: string[] = CAUSE_OPTIONS
   private countryOptions: string[] = countries.map((c: { name: string }) => c.name)
   private projectTypeOptions: string[] = PROJECT_TYPE_OPTIONS.slice(0)
+  private STEP_1 = STEP_1
+  private STEP_2 = STEP_2
 
   @Action private storeProjectFormData: (project: Project) => void
   @Action private createProject: (project: Project) => Promise<void>
@@ -165,17 +183,37 @@ export default class CreateProjectPage extends Vue {
   private created() {
     this.fileName = createID()
     Object.assign(this.project, this.getSavedCreateProjectFormData)
+    window.onpopstate = this.loadURLStep.bind(this)
+    this.loadURLStep()
   }
 
   private cancel() {
     this.$router.push('/')
   }
+
   private nextStep() {
-    this.step++
+    if (STEPS[this.step]) {
+      this.step++
+      this.pushURLStep()
+    }
   }
 
   private prevStep() {
-    this.step--
+    if (STEPS[this.step - 2]) {
+      this.step--
+      this.pushURLStep()
+    }
+  }
+
+  private loadURLStep() {
+    const hash = window.location.hash.substring(1)
+    this.step = hash && STEPS.includes(hash) ? STEPS.indexOf(hash) + 1 : 1
+  }
+
+  private pushURLStep() {
+    const { step } = this
+    const hash = `#${STEPS[this.step - 1]}`
+    history.pushState(null, '', hash)
   }
 
   get validations() {
@@ -187,7 +225,8 @@ export default class CreateProjectPage extends Vue {
       title,
       description,
       projectType,
-      email
+      email,
+      state
     } = this.project
     return {
       // step 1
@@ -196,15 +235,16 @@ export default class CreateProjectPage extends Vue {
         organizationType: required(organizationType),
         organizationDescription:
           required(organizationDescription) &&
-          minLength(organizationDescription, 50) &&
-          maxLength(organizationDescription, 255),
-        country: required(country)
+          minLength(organizationDescription, 10) &&
+          maxLength(organizationDescription, 200),
+        country: required(country),
+        state: required(state)
       },
       // step 2
       [STEP_2]: {
-        title: required(title) && minLength(title, 10) && maxLength(title, 255),
+        title: required(title) && minLength(title, 10) && maxLength(title, 200),
         projectType: required(projectType),
-        description: required(description) && minLength(description, 50),
+        description: required(description) && minLength(description, 100),
         email: required(email) && isEmail(email)
       }
     }
@@ -213,7 +253,7 @@ export default class CreateProjectPage extends Vue {
   get isValid() {
     return {
       [STEP_1]: Object.values(this.validations[STEP_1]).every((v) => !!v),
-      [STEP_2]: Object.values(this.validations[STEP_1]).every((v) => !!v)
+      [STEP_2]: Object.values(this.validations[STEP_2]).every((v) => !!v)
     }
   }
 
@@ -240,12 +280,12 @@ export default class CreateProjectPage extends Vue {
   }
 
   &__Step {
-    margin: 2rem 0 2rem;
+    margin: 2rem 0 1.5rem;
     align-items: center;
     justify-content: space-between;
     display: flex;
     border-bottom: $colorGray dotted 1px;
-    padding-bottom: 1.5rem;
+    padding-bottom: 1rem;
   }
 
   &__Actions {
