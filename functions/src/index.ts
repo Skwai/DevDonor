10bf2bc8f340cd9f9cabc5684e9507a449d2bb0c
Cameron <cameron@biteable.com>
2018-03-03 17:21:43 +1100

    app upload thumbnail generation all working

--- functions/src/index.ts
+++
@@ -1,8 +0,0 @@
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-
-import * as functions from 'firebase-functions'
-
-import generateThumbnailFunction from './generate-thumbnail'
-
-export const generateThumbnail = generateThumbnailFunction
