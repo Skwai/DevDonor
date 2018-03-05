676ce5c8568b132421f19d3b5411e29614427aad
Cameron <cameron@biteable.com>
2018-03-05 23:49:52 +1100

    Responsive project preview

--- src/components/ProjectPreview.vue
+++ src/components/ProjectPreview.vue
@@ -5,27 +5,26 @@
   >
     <div :class="$style.ProjectPreview__LogoWrap">
       <img :class="$style.ProjectPreview__Logo" :src="project.organizationLogo">
+
     </div>
     <div :class="$style.ProjectPreview__Body">
+      <div :class="$style.ProjectPreview__Tags">
+        <AppTag>{{projectType}}</AppTag>
+      </div>
       <header :class="$style.ProjectPreview__Header">
         <h2 :class="$style.ProjectPreview__Title">{{project.title}}</h2>
-        <div :class="$style.ProjectPreview__Tags">
-          <AppTag>{{projectType}}</AppTag>
-          <AppLabel v-if="isNew">New</AppLabel>
-        </div>
+
+        <time :class="$style.ProjectPreview__CreatedAt" :datetime="project.createdAt">
+          <AppGlyph><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm.5-13H11v6l5.25 3.15.75-1.23-4.5-2.67z"></path></svg></AppGlyph>
+          <span>{{createdAt}}</span>
+        </time>
       </header>
-      <div>
-      </div>
       <div :class="$style.ProjectPreview__Meta">
         <div :class="$style.ProjectPreview__Organization">{{project.organizationName}}</div>
         <div :class="$style.ProjectPreview__Region">
           <AppGlyph><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg></AppGlyph>
           <span>{{countryName}}</span>
         </div>
-        <time :class="$style.ProjectPreview__CreatedAt" :datetime="project.createdAt">
-          <AppGlyph><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm.5-13H11v6l5.25 3.15.75-1.23-4.5-2.67z"></path></svg></AppGlyph>
-          <span>Created {{createdAt}}</span>
-        </time>
       </div>
     </div>
   </router-link>
@@ -36,7 +35,6 @@ import moment from 'moment'
 import { Component, Prop, Vue } from 'vue-property-decorator'
 import { Getter } from 'vuex-class'
 import IProjectProperties from '../interfaces/ProjectProperties'
-import { NEW_PROJECT_DAYS } from '../config'
 
 @Component
 export default class ProjectPreview extends Vue {
@@ -58,18 +56,12 @@ export default class ProjectPreview extends Vue {
     }
   }
 
-  get isNew() {
-    if (!this.project.createdAt) {
-      return false
-    }
-    return (
-      new Date(this.project.createdAt).getTime() >
-      new Date().getTime() - NEW_PROJECT_DAYS * 24 * 60 * 60 * 1000
-    )
-  }
-
   get createdAt() {
-    return moment(this.project.createdAt).fromNow()
+    const days = moment().diff(this.project.createdAt, 'days')
+    if (days === 0) {
+      return 'Today'
+    }
+    return `${days}d ago`
   }
 
   get skills() {
@@ -87,20 +79,26 @@ export default class ProjectPreview extends Vue {
 .ProjectPreview {
   $logoSize = 5rem;
   width: 100%;
-  display: grid;
-  grid-gap: $spacingBase;
-  grid-template-columns: $logoSize 1fr;
   color: inherit;
+  display: block;
   flex-direction: row;
   background: #fff;
   transition: $transitionBase;
-  padding: $spacingBase;
   width: 100%;
   -webkit-backface-visibility: hidden;
   position: relative;
   card();
   transform-origin: center center;
   will-change: box-shadow;
+  display: grid;
+  padding: $spacingBase;
+  grid-gap: $spacingBase;
+  grid-template-columns: $logoSize 1fr;
+
+  @media (min-width: 768px) {
+    padding: $spacingBase;
+    grid-gap: $spacingBase;
+  }
 
   &:hover, &:focus {
     box-shadow: rgba(0, 0, 0, 0.2) 0 2px 2rem;
@@ -149,6 +147,11 @@ export default class ProjectPreview extends Vue {
     font-weight: 600;
     line-height: 1.35;
     transition: $transitionBase;
+    margin: 0;
+
+    @media (max-width: 767px) {
+      font-size: 1rem;
+    }
   }
 
   &__Body {
@@ -173,25 +176,27 @@ export default class ProjectPreview extends Vue {
     margin-bottom: 0.75rem;
   }
 
-  &__New {
-    margin-right: $spacingBase;
-  }
-
-  &__Tags {
-    margin-left: auto;
-    padding-left: $spacingBase;
-  }
-
   &__CreatedAt {
+    position: absolute;
+    right: $spacingBase;
+    top: $spacingBase;
     display: flex;
     align-items: center;
-    margin-left: $spacingBase;
+    // margin-left: auto;
+    color: rgba($colorDarkBlue, 0.5);
+    white-space: nowrap;
+    // padding-left: $spacingBase;
+    font-size: $fontSizeSmall;
   }
 
   &__Region {
     margin-left: 1.5rem;
     display: flex;
     align-items: center;
+  }
+
+  &__Tags {
+    margin-bottom: 0.75rem;
   }
 }
 </style>
