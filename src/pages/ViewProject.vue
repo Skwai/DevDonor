01aeba2b619179559741afcc621603eacb602eae
Cameron <cameron@biteable.com>
2018-03-05 23:50:17 +1100

    Better view project created date format

--- src/pages/ViewProject.vue
+++ src/pages/ViewProject.vue
@@ -11,7 +11,7 @@
           <AppGlyph>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm.5-13H11v6l5.25 3.15.75-1.23-4.5-2.67z"></path></svg>
           </AppGlyph>
-          Created {{createdAt}}
+          {{createdAt}}
         </div>
         <div :class="$style.ViewProject__Region">
           <AppGlyph>
@@ -133,9 +133,16 @@ export default class ViewProjectPage extends Vue {
   }
 
   get createdAt() {
-    if (this.project) {
-      return moment(this.project.createdAt).fromNow()
+    if (!this.project) {
+      return null
     }
+
+    const days = moment().diff(this.project.createdAt, 'days')
+
+    if (days === 0) {
+      return 'Today'
+    }
+    return `${days}d ago`
   }
 
   get description(): string | null {
